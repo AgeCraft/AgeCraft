05eb9496a8460730c93f165e9136680ade962a32
DanieHuisman <danielhuisman.development@gmail.com>
2015-06-17 12:20:29 +0200

    Fixed dependency issues

--- src/main/java/org/agecraft/ACReference.java
+++ src/main/java/org/agecraft/ACReference.java
@@ -5,8 +5,8 @@ public class ACReference {
 	public static final String MOD_ID = "AgeCraft";
 	public static final String NAME = "AgeCraft";
 	public static final String VERSION = "@VERSION@";
-	public static final String MC_VERSION = "[@MC_VERSION@]";
-	public static final String DEPENDENCIES = "required-after:Forge@[11.14.1.1334,);required-after:CodeChickenCore@[1.0.5.34,);required-after:LLibrary@[0.2.0-1.8,);";
-	public static final String CLIENT_PROXY = "org.agecraft.ACCientProxy";
+	public static final String MC_VERSION = "[1.8]";
+	public static final String DEPENDENCIES = "required-after:Forge@[11.14.1.1334,);required-after:CodeChickenCore@[1.0.5.34,);required-after:llibrary@[0.2.0-1.8,);";
+	public static final String CLIENT_PROXY = "org.agecraft.ACClientProxy";
 	public static final String SERVER_PROXY = "org.agecraft.ACServerProxy";
 }
