f1076767b48bac93168e250223d67c7c8c15be48
Daniel Huisman <danielhuisman.development@gmail.com>
2015-12-01 21:47:16 +0100

    Updated to latest ExtendedMetadata and updated dependency requirements

--- src/main/java/org/agecraft/ACReference.java
+++ src/main/java/org/agecraft/ACReference.java
@@ -5,8 +5,8 @@ public class ACReference {
     public static final String MOD_ID = "AgeCraft";
     public static final String NAME = "AgeCraft";
     public static final String VERSION = "@VERSION@";
-    public static final String MC_VERSION = "[1.8]";
-    public static final String DEPENDENCIES = "required-after:Forge@[11.14.1.1334,);required-after:CodeChickenCore@[1.0.5.34,);required-after:llibrary@[0.3.0-1.8,);";
+    public static final String MC_VERSION = "[1.8.8]";
+    public static final String DEPENDENCIES = "required-after:Forge@[11.15.0.1591,);required-after:llibrary@[0.6.1,);required-after:ExtendedMetadata@[1.4.3,);";
     public static final String CLIENT_PROXY = "org.agecraft.ACClientProxy";
     public static final String SERVER_PROXY = "org.agecraft.ACCommonProxy";
 
